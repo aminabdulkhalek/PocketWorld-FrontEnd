f1d039773abe6ca462fc0cd5b05cb0d19592ce4b
Karim <abdelkarim.kh95@gmail.com>
2022-02-15 17:05:51 +0200

    getting username on home page

--- scripts/script-home.js
+++ scripts/script-home.js
@@ -1,12 +1,13 @@
 const logout = document.getElementById("logout");
 logout.addEventListener("click", function () {
+  localStorage.clear();
   location.href = "../index.html";
 });
 
 // user info though local storage
-// const id = localStorage.getItem("user_id");
-// const firstName_stored = localStorage.getItem("first_name");
-// const lastName_stored = localStorage.getItem("last_name");
+const id = localStorage.getItem("user_id");
+const firstName_stored = localStorage.getItem("first_name");
+const lastName_stored = localStorage.getItem("last_name");
 
-// const userName = document.getElementById("username");
-// userName.textContent = firstName_stored.toUpperCase() + " " + lastName_stored.toUpperCase();
+const userName = document.getElementById("username");
+userName.textContent = firstName_stored + " " + lastName_stored;
